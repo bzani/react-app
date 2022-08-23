911c8c827544040eef7b33a3c1a8f6601f3dbf05
bzani <ubzani@gmail.com>
2022-08-23 21:17:52 +0100

    using component and props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className='card'>
-        <h2>Delete feature</h2>
-        <div className='actions'>
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <Todo text='Learn React' />
+      <Todo text='Code React' />
+      <Todo text='Test React' />
     </div>
   );
 }
