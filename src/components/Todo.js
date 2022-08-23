d7bf3a45f5d1bb5670229972801e18071a3b7a45
bzani <ubzani@gmail.com>
2022-08-23 21:26:51 +0100

    handling events

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,25 @@
+// must use capital letter for component function
 function Todo(props) {
+
+    // calling with () in 'onClick' below would
+    //   make the function to be executed whenever
+    //   javascript evaluates the html code there 
+    //   (component return block)
+    // to execute when the element is clicked then
+    //   we should call it simply by function name
+    function deleteHandler() {
+        console.log("clicked!");
+        console.log(props.text);
+    }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
         <span>Click here to delete</span>
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
     </div>
   );
